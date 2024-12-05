6a294a187c8f15671d2759dcee937a64e0d15a40
vokewasike <vokewasike@gmail.com>
2024-12-05 17:15:27 +0000

    updated static and media root folders

--- app/settings.py
+++ app/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "public" / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "app" / "static",
@@ -96,7 +96,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = BASE_DIR / "public" / "media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Internationalization
